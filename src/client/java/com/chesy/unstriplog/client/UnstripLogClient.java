f6eeb230a1437773e7ef17d00bd852c7aca9a827
Chesy <chestedgt@gmail.com>
2025-05-07 01:12:51 +0800

    -

---
+++ src/client/java/com/chesy/unstriplog/client/UnstripLogClient.java
@@ -0,0 +1,10 @@
+package com.chesy.unstriplog.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnstripLogClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/chesy/unstriplog/UnstripLog.java
@@ -0,0 +1,10 @@
+package com.chesy.unstriplog;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UnstripLog implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
