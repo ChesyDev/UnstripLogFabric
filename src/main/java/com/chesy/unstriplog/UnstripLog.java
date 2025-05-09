11c7131d3e881efbdc9a33cef85cb3e33d6282c1
Chesy <chestedgt@gmail.com>
2025-05-10 04:31:02 +0800

    Now working for stem and hyphae

--- src/main/java/com/chesy/unstriplog/UnstripLog.java
+++ src/main/java/com/chesy/unstriplog/UnstripLog.java
@@ -32,8 +32,9 @@ public class UnstripLog implements ModInitializer {
             Identifier id = Registries.BLOCK.getId(block);
             String path = id.getPath();
 
-            if ((path.endsWith("_log") || path.endsWith("_wood")) || path.endsWith("stem") || path.endsWith("hyphae") && !path.startsWith("stripped_")) {
+            if ((path.endsWith("_log") || path.endsWith("_wood") || path.endsWith("stem") || path.endsWith("hyphae")) && !path.startsWith("stripped_")) {
                 LOGS.add(block);
+                System.out.println(path);
             }
         }
 
