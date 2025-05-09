cfcd1a0793e570ed071facf12896b35f026b2c2d
Chesy <chestedgt@gmail.com>
2025-05-10 04:38:16 +0800

    - Added support for hyphae and stem - Fixed log axis getting reset after using bark

--- src/main/java/com/chesy/unstriplog/item/BarkItem.java
+++ src/main/java/com/chesy/unstriplog/item/BarkItem.java
@@ -3,6 +3,7 @@ package com.chesy.unstriplog.item;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.registry.Registries;
@@ -11,6 +12,7 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 import java.util.HashMap;
@@ -28,7 +30,7 @@ public class BarkItem extends Item {
             Identifier id = Registries.BLOCK.getId(block);
             String path = id.getPath(); // e.g. "stripped_oak_log"
 
-            if (path.startsWith("stripped_") && (path.endsWith("_log") || path.endsWith("_wood"))) {
+            if (path.startsWith("stripped_") && (path.endsWith("_log") || path.endsWith("_wood") || path.endsWith("stem") || path.endsWith("hyphae"))) {
                 // Make sure the length of the path is greater than "stripped_" to avoid substring error
                 if (path.length() > "stripped_".length()) {
                     String originalPath = path.substring("stripped_".length());
@@ -56,7 +58,14 @@ public class BarkItem extends Item {
         Block reversed = REVERSE_STRIPPED.get(targetState.getBlock());
         if (reversed != null) {
             if (!world.isClient) {
-                world.setBlockState(pos, reversed.getDefaultState());
+                BlockState newState = reversed.getDefaultState();
+
+                if (newState.contains(PillarBlock.AXIS) && targetState.contains(PillarBlock.AXIS)) {
+                    Direction.Axis axis = targetState.get(PillarBlock.AXIS);
+                    newState = newState.with(PillarBlock.AXIS, axis);
+                }
+
+                world.setBlockState(pos, newState, 3);
                 world.playSound(null, pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
 
                 // Consume 1 bark item
