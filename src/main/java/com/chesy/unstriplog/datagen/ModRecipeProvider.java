884d08d90dd3a1183c9163b4a7d2bbdaf9221534
CoolerProRB <118531502+CoolerProRB@users.noreply.github.com>
2025-05-21 11:47:04 +0800

    v1.0.1-1.21.3-Fixed bark to paper recipe overriding vanilla paper recipe

--- src/main/java/com/chesy/unstriplog/datagen/ModRecipeProvider.java
+++ src/main/java/com/chesy/unstriplog/datagen/ModRecipeProvider.java
@@ -41,7 +41,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                         .pattern("##")
                         .input('#', ModItems.BARK)
                         .criterion(getHasName(ModItems.BARK), has(ModItems.BARK))
-                        .offerTo(exporter);
+                        .offerTo(exporter, "paper_from_bark");
             }
 
             private String getItemName(ItemConvertible item) {
